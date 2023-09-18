75eb656fbd906573071b6470941559b85bb973d2
Jack Harvey <jack.harvey1019@gmail.com>
2023-09-18 09:34:55 -0700

    added display_menu and opt skeleton

--- adventure.py
+++ adventure.py
@@ -12,7 +12,30 @@ def input_integer(prompt: str, lower_lim: int=-m.inf, upper_lim: int=m.inf) -> i
         except ValueError:
             print(f"Please enter an int.")
 
+def display_menu():
+    print("\nYou are in a concrete room barely taller than you. There is an air duct, a grate, and a very small nightstand.")
+    print("On the nightstand is a lamp, a shoestring, and a paper clip. What do you want to do?")
+    print("1  Inspect the stuff on the nightstand")
+    print("2  Find a way out")
+    print("3  Stare blankly at the wall\n")
+
+def opt_1():
+    pass
+
+def opt_2():
+    pass
+
+def opt_3():
+    print("\nYou stare blankly at the wall. What did you expect?")
+
 def main():
-    print(input_integer("Enter an int between 1 and 3: ", 1, 3))
+    display_menu()
+    choice = input_integer("What do you want to do? ", 1, 3)
+    if choice == 1:
+        opt_1()
+    elif choice == 2:
+        opt_2()
+    elif choice == 3:
+        opt_3()
 
 main()
