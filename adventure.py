9c8cff501b0b12cc3aef75d9c281c00d7ebca38b
Jack Harvey <jack.harvey1019@gmail.com>
2023-09-25 16:46:14 -0700

    added print_options, updated funcs

--- adventure.py
+++ adventure.py
@@ -15,67 +15,79 @@ def input_integer(prompt: str, lower_lim: int=-m.inf, upper_lim: int=m.inf) -> i
             print("\nExiting program...")
             exit()
 
-def display_menu():
+def display_menu() -> None:
     menu = "\nYou are in a concrete room barely taller than you. "
     menu += "There is an air duct, a grate in the floor, and a very small nightstand. "
     menu += "On the nightstand is a lamp, a shoestring, and a paperclip. What do you want to do? "
-    menu += "\n1  Inspect the stuff on the nightstand"
-    menu += "\n2  Find a way out"
-    menu += "\n3  Stare blankly at the wall"
     menu += "\n Type 'exit' to give up."
     print(menu)
+    print_options("Inspect the stuff on the nightstand", "Find a way out", "Stare blankly at the wall")
 
+def print_options(*options) -> None:
+    for i, option in enumerate(options):
+        print(f"{i+1}. {option}")
+    
+# Satifies 12
 def inspect_nightstand(inventory: list, objs: list=None) -> list:
     if objs is None:
-        print("You did not enter the objects to be used.")
+        print("Objs to be entered cannot be default.")
         exit()
 
-    options = "\nOn the nightstand is a lamp, a shoestring, and a paperclip. What do you want to do? "
-    options += "\n1 Take the lamp"
-    options += "\n2 Take the shoestring"
-    options += "\n3 Take the paperclip"
-    options += "\n4 Take all three\n"
-    options += "\n What do you want to do? "
-    choice = input_integer(options, 1, 4)
+    print("\nOn the nightstand is a lamp, a shoestring, and a paperclip. What do you want to do?")
+    print_options("Take the lamp", "Take the shoestring", "Take the paperclip", "Take all three")
+    choice = input_integer("\t->", 1, 4)
 
-    if choice == 1:
+    if choice == 1 and "lamp" not in inventory:
        inventory, objs = update_inventory(inventory, objs, "lamp")
-    elif choice == 2:
+    elif choice == 2 and "shoestring" not in inventory:
         inventory, objs = update_inventory(inventory, objs, "shoestring")
-    elif choice == 3:
+    elif choice == 3 and "paperclip" not in inventory:
         inventory, objs = update_inventory(inventory, objs, "paperclip")
-    elif choice == 4:
+    elif choice == 4 and objs is not []:
         inventory, objs = update_inventory(inventory, objs, None)
+    else:
+        print("\nThe item you would like is no longer on the nightstand.")
 
-    print(inventory)
-    print(objs)
-    return inventory
+    return inventory, objs
 
 def leave_room(inventory: list):
-    options = "\nYou have only two choices here."
-    options += "\n1 Try to crawl through the air duct"
-    options += "\n2 Try to unscrew the grate"
-
-    choice = input_integer(options, 1, 2)
-    if choice == 1 and "lamp" in inventory:
-        pass
-    elif choice == 1 and "shoestring" in inventory:
-        pass
-    elif choice == 1 and "paperclip" in inventory:
-        pass
-    elif choice == 2:
-        pass
+    print("\nYou have only two choices here.")
+    print_options("Try to crawl through the air duct", "Try to unscrew the grate")
+    choice = input_integer("\t-> ", 1, 2)
+    
+    if len(inventory) == 1:
+        if choice == 1 and "lamp" in inventory:
+            lamp_choice = "\nYou easily enter the duct, but it is pitch black. "
+            lamp_choice += "When you finally find an outlet to plug in the lamp, the bulb glows brightly. "
+            lamp_choice += "Unfortunately, it is so bright that Siren Head finds you and gobbles you up."
+            print(lamp_choice)
+        elif choice == 1 and "shoestring" in inventory:
+            lamp_choice = "\nYou have three shoelaces (2 xshoes, 1x), an odd number... "
+            lamp_choice += "Naturally, you are teleported to the third dimension and become the z-coordinate on a 3D math problem."
+            print(lamp_choice)
+        elif choice == 1 and "paperclip" in inventory:
+            clip_choice = "\nWhew! That was a close one! You scramble through the duct. "
+            clip_choice += "The paperclip didn't really do anything, but it was the only safe choice. "
+            clip_choice += "So, good job!"
+            print(clip_choice)
+        elif choice == 2:
+            print("\nThe grate is hard to unscrew, so you pound your foot on it. You fall through and die.")
+    elif len(inventory) == 0:
+        print("\nYou are so light that you float out of the room and asphyxiate due to high-altitude oxygen loss.")
+    elif len(inventory) > 1 and choice == 1:
+        print("\nYou are too heavy, so the moment your finger touches the grate, you implode and form a supermassive black hole.")
+    elif len(inventory) > 1 and choice == 2:
+        print("\nYou are too heavy, so you fall straight through the thin aluminum of the air duct onto the hard floor.")
 
 def update_inventory(inventory: list, objs: list, add_objs: str) -> list:
     if add_objs is not None:
         inventory.append(add_objs)
         objs.remove(add_objs)
     else:
-        inventory.append(objs)
-        objs.clear()
+        inventory.extend(objs)
     return inventory, objs
 
-def stare():
+def stare() -> None:
     print("\nYou stare blankly at the wall. What did you expect?")
 
 def main():
@@ -86,9 +98,9 @@ def main():
 
     while choice != "exit":
         if choice == 1:
-            inventory = inspect_nightstand(inventory, objs)
+            inventory, objs = inspect_nightstand(inventory, objs)
         elif choice == 2:
-            inventory, objs = leave_room(inventory)
+            leave_room(inventory)
             break
         elif choice == 3:
             stare()
